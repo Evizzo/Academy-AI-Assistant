efcd69a86bd0ccd7620f280b03e1f447c9aa3ac6
evizzo <stefanmatic941@gmail.com>
2025-04-10 19:22:14 +0200

    mysql

---
+++ db.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import mysql.connector
+
+def getDbConnection():
+    return mysql.connector.connect(
+        host=st.secrets["mysql"]["host"],
+        port=st.secrets["mysql"]["port"],
+        database=st.secrets["mysql"]["database"],
+        user=st.secrets["mysql"]["user"],
+        password=st.secrets["mysql"]["password"]
+    )
